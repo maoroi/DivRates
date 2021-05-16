cbc84f5218c0831b5f9f2bcbadc4ff8e37230178
RM <roeemaor@gmail.com>
2021-05-16 22:21:19 +0100

    starting to process results

--- MuHiSSE/5-Processing cluster output.R
+++ MuHiSSE/5-Processing cluster output.R
@@ -6,28 +6,29 @@ extRDS <- function(file){
     res <- tmp[c(1:3)]
 }
 
+# extract the first 3 elements of each model into a data frame
 allmods <- list.files(pattern = ".RDS")
 modat <- data.frame(t(sapply(allmods, extRDS)))
-score <- do.call(cbind, lapply(modat[,1:3], as.numeric))
-modat$type <- NA
-modat$states <- NA
-modat$tree <- NA
+score <- as.data.frame(do.call(cbind, lapply(modat[,1:3], as.numeric)))
+rownames(score) <- rownames(modat)
+score$type <- NA
+score$states <- NA
+score$tree <- NA
 
 #trees <- c("MCC", "0028", "1166", "1845", "2966", "3865", "4496", "5221", "5333", "5455", "6404", "7198", "9128")
 
 # extracting model parameters from file name
-for (i in 1:nrow(modat)){
-    modat$type[i] <- gsub("[0-9]", "", strsplit(rownames(modat[i,]),"_")[[1]][1])       # model type
-    modat$states[i] <- gsub("[^0-9]", "", strsplit(rownames(modat[i,]),"_")[[1]][1])    # no. of hidden states
+for (i in 1:nrow(score)){
+    score$type[i] <- gsub("[0-9]", "", strsplit(rownames(score[i,]),"_")[[1]][1])       # model type
+    score$states[i] <- gsub("[^0-9]", "", strsplit(rownames(score[i,]),"_")[[1]][1])    # no. of hidden states
     if (i %% 13 == 1) {
-        modat$tree[i] <- "MCC"                                                          # tree variant
+        score$tree[i] <- "MCC"                                                          # tree variant
     } else {
-        modat$tree[i] <- gsub("[^0-9]", "", strsplit(rownames(modat[i,]),"_")[[1]][2])
+        score$tree[i] <- gsub("[^0-9]", "", strsplit(rownames(score[i,]),"_")[[1]][2])
     }
 } 
-
-rownames(modat) <- NULL
-write.csv(as.data.frame(modat), file="modat.csv")
+rownames(score) <- NULL
+write.csv(score, file="score.csv")
 
 
 
