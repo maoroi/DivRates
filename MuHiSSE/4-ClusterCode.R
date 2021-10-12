6428866cb953dce960dfed0ceaeec5fd1f00a304
RM <roeemaor@gmail.com>
2021-10-12 22:29:52 +0100

    Additional analyses moved to another file; hopefully this one still works

--- MuHiSSE/4-ClusterCode.R
+++ MuHiSSE/4-ClusterCode.R
@@ -3,22 +3,65 @@ library(phytools)
 
 # 1. Preliminary data ---------------------------------------------------------
 # (this is a randomisation section and, to save resources, doesn't have to be re-run every time) 
-
+set.seed(88)
 
 #indices <- read.csv(file="Indices_of_5k_vars_used_for_MCC.csv")
 #to_use <- sample(1:5000, 24, replace=FALSE) # choose 24 of the 5k trees used in the MCC
 #tree_no <- sprintf("%04d",indices[to_use,1])
-##write.csv(tree_no, file="Indices_of_24Semi-finalists.csv")
+#write.csv(tree_no, file="Indices_of_24Semi-finalists.csv")
 #tree_no <- tree_no[c(2,4,6,8,10,12,14,16,18,20,22,24)] # to halve number of repeats (if needed)
-#
-## get trees and remove crepuscular tips
-#for (i in 1:length(tree_no)){
-#    phyfile <- paste0("C:/Users/Roi Maor/Desktop/New Mam Phylo/Completed_5911sp_topoCons_NDexp/MamPhy_BDvr_Completed_5911sp_topoCons_NDexp_v2_tree",tree_no[i],".tre")
-#    treevar <- read.tree(phyfile)
-#    treevar <- CorTax(treevar)
-#    treevar <- drop.tip(treevar, which(!treevar$tip.label %in% act3$Phylo_name))
-#    write.tree(treevar, file=paste0('tree',tree_no[i],'.nex'))
-#}
+
+
+# additional variants to increase sampling in MuHiSSE3 and MuHiSSE4 models
+used <- read.csv(file="Indices_of_24Semi-finalists.csv")
+used <- sprintf("%04d",used$x)
+to_use <- sprintf("%04d", sample(1:9999, 100, replace=FALSE)) # choose 48 trees using 4-digit format
+# loop to ensure new sample doesn't introduce duplicates
+while(length(which(to_use %in% used)) > 0) {
+    to_add <- length(which(to_use %in% used))                           
+    addsamp <- sprintf("%04d", sample(1:9999, to_add, replace=FALSE))   # additional sample 
+    to_use <- c(to_use, addsamp)                        
+    to_use <- unique(to_use[-which(to_use %in% used)])                  # remove duplicates
+}
+tree_no <- to_use
+
+# loading activity data here to enable the following function
+data <- read.csv("ActivityData_MDD_v1_match.csv")   # file made in "2-Preparing for MuHiSSE.r"
+
+# function to correct binomials
+CorTax <- function(tree){
+    tree$tip.label[which(tree$tip.label == "Equus_africanus")]  <- "Equus_asinus"   # asinus is domestic E. africanus
+    tree$tip.label[which(tree$tip.label == "Pygeretmus_zhitkovi")]  <- "Pygeretmus_shitkovi"    # correct spelling
+    tree$tip.label[which(tree$tip.label == "Aethomys_namaquensis")] <- "Micaelamys_namaquensis" # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Tamiops_macclellandii")]<- "Tamiops_mcclellandii"   # typo
+    tree$tip.label[which(tree$tip.label == "Nesotragus_moschatus")] <- "Neotragus_moschatus"    # typo
+    tree$tip.label[which(tree$tip.label == "Pseudalopex_culpaeus")] <- "Lycalopex_culpaeus"     # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Pseudalopex_griseus")]  <- "Lycalopex_griseus"      # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Pseudalopex_gymnocercus")]  <- "Lycalopex_gymnocercus"  # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Pseudalopex_gymnocercus")]  <- "Lycalopex_sechurae" # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Pseudalopex_vetulus")]  <- "Lycalopex_vetulus"      # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Lutra_maculicollis")]   <- "Hydrictis_maculicollis" # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Monodelphis_unistriatus")]  <- "Monodelphis_unistriata" # Latin grammar correction
+    tree$tip.label[which(tree$tip.label == "Dactylonax_palpator")]  <- "Dactylopsila_palpator"  # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Elephantulus_revoilii")]<- "Elephantulus_revoili"   # spelling error
+    tree$tip.label[which(tree$tip.label == "Zaglossus_bruijnii")]   <- "Zaglossus_bruijni"      # spelling error
+    tree$tip.label[which(tree$tip.label == "Procolobus_badius")]    <- "Piliocolobus_badius"    # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Procolobus_kirkii")]    <- "Piliocolobus_kirkii"    # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Procolobus_preussi")]   <- "Piliocolobus_preussi"   # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Procolobus_pennantii")] <- "Piliocolobus_pennantii" # genus transfer v1
+    tree$tip.label[which(tree$tip.label == "Procolobus_rufomitratus")]  <- "Piliocolobus_rufomitratus"   # genus transfer v1
+    tree <- drop.tip(tree, which(!tree$tip.label %in% data$Phylo_name))
+}
+
+# get trees and remove crepuscular tips
+creps <- data$Phylo_name[which(data$AP == 'Crepuscular')]           
+for (i in 1:length(tree_no)){
+    phyfile <- paste0("C:/Users/Roi Maor/Desktop/New Mam Phylo/Completed_5911sp_topoCons_NDexp/MamPhy_BDvr_Completed_5911sp_topoCons_NDexp_v2_tree",tree_no[i],".tre")
+    treevar <- read.tree(phyfile)
+    treevar <- CorTax(treevar)
+    treevar <- drop.tip(treevar, creps)
+    write.tree(treevar, file=paste0('tree',tree_no[i],'.nex'))
+}
 
 
 # 2. Make files for MCC NULL and MuSSE models ---------------------------------
@@ -898,7 +941,7 @@ for (n in 1:4){
     #dir.create(file.path(paste0("./Analyses/VR/VR_CID/vrCID",n+1)))
     
 
-    ## 4.1 Files for MCC analyses -------------------------------------------------
+    ## 4.1 Files for MCC analyses ---------------------------------------------
 
     ## MuHiSSE Shell file
     # this format is used to avoid the DOS (^M) end-of-line which trips the cluster. End-of-line DOS characters can be 
@@ -1133,7 +1176,7 @@ tmp <- MuHiSSE(phy=tree, data=states_trans, f=freq,
         sink()
     }
 
-    ## 4.2 Generate files for 12 tree variants ------------------------------------
+    ## 4.2 Generate files for 12 tree variants --------------------------------
 
     for (k in 1:length(tree_no)){
         # MuHiSSE Shell file
@@ -1367,6 +1410,11 @@ tmp <- MuHiSSE(phy=tree, data=states_trans, f=freq,
     }
 } 
 
+
+
+# 5. Creating files for additional analyses -----------------------------------
+
+}
 
 ##################################################################################################################
 ###  R template to replicate for running on the cluster  ####
